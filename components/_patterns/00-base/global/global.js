0acaf1612b942e4912df42527cdd8a6bd9961d61
Evan Willhite <evan@evanwillhite.com>
2017-02-06 09:49:14 -0600

    Update 2017-02-06T15:49:11.817Z

--- components/_patterns/00-base/global/global.js
+++
@@ -1,9 +0,0 @@
-// Example Global javascript
-
-// If using Typekit
-// try {
-//   Typekit.load();
-// }
-// catch (e) {
-//   alert('An error has occurred: ' + e.message);
-// }
--- components/_patterns/01-atoms/06-buttons/button.md
+++
@@ -1,7 +0,0 @@
----
-el: ".button"
-title: "Button Styling"
----
-## Button Styling
-
-Default button styling with an alternative variation
